bbebf24180f7371039e5690cb65ac614bf6026d4
alainkueng <45451714+alainkueng@users.noreply.github.com>
2019-10-03 16:37:31 +0200

    Player class modified

--- Snakes and Ladders/src/Player.java
+++ Snakes and Ladders/src/Player.java
@@ -1,8 +1,23 @@
-import java.util.List;
-
-public class Player {
 
 
+public class Player { // Attribute
+    String name;
+    int pos;
+
+
+    public Player() { //the constructor
+        this.pos = 0;  //position on board, starting at 0
+    }
+
+    public void setName(int i) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Player " + i + ":");
+        name = s.nextLine();
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
 
 
