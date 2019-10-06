a2340c071fa65343d5e7a3774b8e8d6d08175a87
Raphael Imfeld <raphael.imfeld@uzh.ch>
2019-10-06 16:05:35 +0200

    Dice: excluded zero

--- Snakes and Ladders/src/Dice.java
+++ Snakes and Ladders/src/Dice.java
@@ -4,7 +4,7 @@ public class Dice {
     private int number;
 
     public int dice(){
-        this.number = ThreadLocalRandom.current().nextInt(0,7);
+        this.number = ThreadLocalRandom.current().nextInt(1,7);
         return number;
     }
 
