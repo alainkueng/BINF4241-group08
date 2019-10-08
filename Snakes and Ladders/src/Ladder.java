412e903bce7831f4fc79d5d2c67e3006c73d1def
alainkueng <45451714+alainkueng@users.noreply.github.com>
2019-10-08 19:42:31 +0200

    repaired snakes ladders

--- Snakes and Ladders/src/Ladder.java
+++ Snakes and Ladders/src/Ladder.java
@@ -11,7 +11,7 @@ public class Ladder extends Square {
     @Override
     public void enter(Player player) {
         super.enter(player);
-        player.move(new_position);
+        player.move(this.position-new_position);
 
     }
     public void settrue(Ladder ladder){
--- Snakes and Ladders/src/Snake.java
+++ Snakes and Ladders/src/Snake.java
@@ -1,13 +1,18 @@
-public class Snake extends Ladder {
+public class Snake extends Square {
 
     int new_position;
 
     public Snake(int number, Game game_obj, int squarenumber) {
-        super(number, game_obj, squarenumber);
+        super(number, game_obj);
         new_position = squarenumber;
     }
 
     public void settrue(Snake snake){
         snake.partner = true;
     }
+    @Override
+    public void enter(Player player) {
+        super.enter(player);
+        player.move(new_position-this.position);
+}
 }
