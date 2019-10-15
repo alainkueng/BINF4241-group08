621e81ae571d2a948651c12b513fc2d8979cf7b3
Raphael Imfeld <raphael.imfeld@uzh.ch>
2019-10-15 20:33:02 +0200

    Board: Implementation of Figures

--- Assignment 2/src/Board.java
+++ Assignment 2/src/Board.java
@@ -1,40 +1,31 @@
-import java.util.List;
-
 public class Board {
 
     public Object[][][] board;
-    public Figure.Type[] figureFirstRow;
 
-    public Board(){
+    public Board() {
         board = new Object[8][8][2];
-        Figure.Type[] figureFirstRow = new Figure.Type[]
-                {Figure.Type.TOWER, Figure.Type.KNIGHT, Figure.Type.BISHOP, Figure.Type.KING, Figure.Type.QUEEN,
-                Figure.Type.BISHOP, Figure.Type.KNIGHT, Figure.Type.TOWER};
         initBoard();
-        initFigure(Figure.Colors.BLACK);
-        initFigure(Figure.Colors.WHITE);
+        initFigure(Figure.Colors.BLACK, 0);
+        initFigure(Figure.Colors.WHITE, 7);
     }
 
-    private void initBoard(){
-        for (int i = 0; i < 8; i++){
-            for (int j = 0; j < 8; j++){
-                if (i % 2 != 0 && j % 2 != 0){
+    private void initBoard() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (i % 2 != 0 && j % 2 != 0) {
                     Square new_square = new Square(Square.Colors.WHITE);
                     board[i][j][0] = new_square;
-                }
-                else if (i % 2 != 0 && j % 2 == 0){
+                } else if (i % 2 != 0 && j % 2 == 0) {
                     Square new_square = new Square(Square.Colors.BLACK);
                     Object[] content = new Object[2];
                     content[0] = new_square;
                     board[i][j][0] = new_square;
-                }
-                else if (i % 2 == 0 && j % 2 != 0){
+                } else if (i % 2 == 0 && j % 2 != 0) {
                     Square new_square = new Square(Square.Colors.BLACK);
                     Object[] content = new Object[2];
                     content[0] = new_square;
                     board[i][j][0] = new_square;
-                }
-                else{
+                } else {
                     Square new_square = new Square(Square.Colors.WHITE);
                     Object[] content = new Object[2];
                     content[0] = new_square;
@@ -43,29 +34,24 @@ public class Board {
             }
         }
     }
-    private void initFigure(Figure.Colors color){
-        if(color == Figure.Colors.BLACK){
 
-        }
-
-    }
-    /*
-    private void initFigure(Figure.Colors color){
-        if(color == Figure.Colors.BLACK){
-            for(int i = 0; i < 8; i++){
-                board[0][i][1] = new Figure(figureFirstRow[i], color);
-            }
-            for(int i = 0; i < 8; i++){
-                board[1][i][1] = new Figure(Figure.Type.PAWN, color);
-            }
+    private void initFigure(Figure.Colors color, int rowNumber) {
+        board[rowNumber][0][1] = new Tower(color);
+        board[rowNumber][1][1] = new Knight(color);
+        board[rowNumber][2][1] = new Bishop(color);
+        board[rowNumber][3][1] = new Queen(color);
+        board[rowNumber][4][1] = new King(color);
+        board[rowNumber][5][1] = new Bishop(color);
+        board[rowNumber][6][1] = new Knight(color);
+        board[rowNumber][7][1] = new Tower(color);
+        if(color == Figure.Colors.BLACK) {
+            rowNumber = 1;
         }
         else{
-            for(int i = 0; i < 8; i++){
-                board[7][i][1] = new Figure(figureFirstRow[i], color);
-            }
-            for(int i = 0; i < 8; i++){
-                board[6][i][1] = new Figure(Figure.Type.PAWN, color);
-            }
+            rowNumber = 6;
         }
-    }*/
+        for(int i = 0; i < 8; i++){
+            board[rowNumber][i][1] = new Pawn(color);
+        }
+    }
 }
