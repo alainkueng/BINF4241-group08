54bf4c32d0c526a0c7aab2133be1c43c503d9954
Raphael Imfeld <raphael.imfeld@uzh.ch>
2019-10-16 16:10:06 +0200

    Game: Name-check fix and constructor only with methods.

--- Assignment 2/src/Game.java
+++ Assignment 2/src/Game.java
@@ -9,35 +9,27 @@ public class Game {
     Game(){
         this.gameboard = new Board();
         this.logic = new Logic();
-        Scanner player1 = new Scanner(System.in);
-        boolean Scan_string_check_1 = false;
-        String whitename = null;
-        while (!Scan_string_check_1){
-            System.out.println("Please enter playername for the white side.");
-            whitename = player1.nextLine();
-            if (whitename.matches("^[a-zA-Züöä]*$")&& !whitename.equals("") && whitename.length() < 15){
-                Scan_string_check_1 = true;
-                break;
+        this.white = new Player(initPlayer(Player.colors.WHITE), Player.colors.WHITE);
+        this.black = new Player(initPlayer(Player.colors.BLACK), Player.colors.BLACK);
+    }
+    private String initPlayer(Player.colors color){
+        Scanner player = new Scanner(System.in);
+        boolean stringCheck = false;
+        String name = null;
+        while (!stringCheck){
+            System.out.printf("Please enter playername for the %s side.\n", color);
+            name = player.nextLine();
+            if (name.matches("^[a-zA-Züöäéàèçëòêâôîï]*$")&& !name.equals("") && name.length() < 15){
+                stringCheck = true;
+                if(color == Player.colors.BLACK && white.getName().equals(name)){
+                    stringCheck = false;
+                    System.out.println("Name already used!");
+                }
             }
-            else{System.out.println("Try Again. Please input characters from A-Z. Max. length 15 letters");
+            else{System.out.println("Please input a name with max. length 15 letters on a Swiss Keyboard");
+            }
         }
-        }
-        Scanner player2 = new Scanner(System.in);
-        boolean Scan_string_check_2 = false;
-        String blackname= null;
-        while (!Scan_string_check_2){
-            System.out.println("Please enter playername for the black side.");
-            blackname = player2.nextLine();
-            if (blackname.matches("^[a-zA-Zöäü]*$")&& !blackname.equals("") && blackname.length() < 15){
-                Scan_string_check_2 = true;
-                break;
-            }
-            else {
-                System.out.println("Try Again. Please input characters from A-Z. Max. length 15 letter");}
-
-            }
-        this.white = new Player(whitename, Player.colors.WHITE);
-        this.black = new Player(blackname, Player.colors.BLACK);
-}
+        return name;
+    }
 }
 
