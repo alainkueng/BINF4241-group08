22d09976b7a6e041a5f0299dbc6f8366fc0ff169
alainkueng <45451714+alainkueng@users.noreply.github.com>
2019-10-16 15:02:17 +0200

    Game() input is now under control

--- Assignment 2/src/Game.java
+++ Assignment 2/src/Game.java
@@ -13,27 +13,27 @@ public class Game {
         boolean Scan_string_check_1 = false;
         String whitename = null;
         while (!Scan_string_check_1){
-            System.out.println("Please enter playername for the white side");
+            System.out.println("Please enter playername for the white side.");
             whitename = player1.nextLine();
-            if (whitename != null && whitename.matches("^[a-zA-Z]*$")){
+            if (whitename.matches("^[a-zA-Z]*$")&& !whitename.equals("")){
                 Scan_string_check_1 = true;
                 break;
             }
-            else{System.out.println("Try Again");
+            else{System.out.println("Try Again. Please input characters from A-Z.");
         }
         }
         Scanner player2 = new Scanner(System.in);
         boolean Scan_string_check_2 = false;
         String blackname= null;
         while (!Scan_string_check_2){
-            System.out.println("Please enter playername for the black side");
+            System.out.println("Please enter playername for the black side.");
             blackname = player2.nextLine();
-            if (blackname!= null && blackname.matches("^[a-zA-Z]*$")){
+            if (blackname.matches("^[a-zA-Z]*$")&& !blackname.equals("")){
                 Scan_string_check_2 = true;
                 break;
             }
             else {
-                System.out.println("Try Again");}
+                System.out.println("Try Again. Please input characters from A-Z.");}
 
             }
         this.white = new Player(whitename, Player.colors.WHITE);
