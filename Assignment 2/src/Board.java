be67942d46a7b35a5b62947065a1745e89f0074f
alainkueng <45451714+alainkueng@users.noreply.github.com>
2019-10-20 16:54:59 +0200

    Merge branch 'Dev' of C:\Users\alain\OneDrive\Desktop\UZH\3. Semester\Software Construction\BINF4241-group08 with conflicts.

--- Assignment 2/src/Board.java
+++ Assignment 2/src/Board.java
@@ -318,8 +318,8 @@ public class Board {
         }
         return checkMate;
     }
-    public static void main(String[] args) {
-        Game game = new Game();
+    public void move(){
+
     }
 }
 
