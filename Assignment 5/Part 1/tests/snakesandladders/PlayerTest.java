371da03387e0d5a4b5498a45c075e3dda4279ce5
alainkueng <45451714+alainkueng@users.noreply.github.com>
2019-12-05 18:46:54 +0100

    Wrote all tests

--- Assignment 5/Part 1/tests/snakesandladders/PlayerTest.java
+++ Assignment 5/Part 1/tests/snakesandladders/PlayerTest.java
@@ -1,8 +1,18 @@
 package snakesandladders;
 
-//public class PlayerTest {
-  //  int to_move = game.checkNumber(5, current_player); //to be sure that it isn't a snake or a ladder
-    ////    current_player.move(to_move);
-        //game.nextPlayer();
-    //assertTrue(game.squares.get(5).player_list.get(0).name == "TestName");// wait i just checked the move method
-//}
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PlayerTest {
+    @Test
+    public void MoveTest() {
+        Game game = new Game();
+        game.createBoard(1,6);
+        game.addPlayerToBoard("TestName");
+        game.numPlayer = 1;
+        Player current_player = game.players.get(game.current);
+        int to_move = game.checkNumber(5, current_player); //to be sure that it isn't a snake or a ladder
+        current_player.move(to_move);
+        assertTrue(game.squares.get(5).player_list.get(0).name == "TestName");
+    }
+}
--- Assignment 5/Part 1/tests/snakesandladders/SnakeTest.java
+++ Assignment 5/Part 1/tests/snakesandladders/SnakeTest.java
@@ -14,5 +14,21 @@ import org.junit.jupiter.api.Test;
 public class SnakeTest {
         @Test
         public void enter() {
+                Game game = new Game();
+                game.createBoard(5,5);
+                game.addPlayerToBoard("TestName");
+                //run() simulated
+                Player current_player = game.players.get(game.current);
+                game.numPlayer = 1;
+                int LadderSearch = -1;
+                for(Square square:game.squares){
+                        LadderSearch += 1;
+                        if(square.getClass() == Snake.class){
+                                break;
+                        }
+                }
+                int to_move = game.checkNumber(LadderSearch, current_player); //to be sure that it isn't a snake or a ladder, LastSquare only has an extra boolean
+                current_player.move(LadderSearch);
+                assertTrue((game.squares.get(((Snake)game.squares.get(LadderSearch)).new_position-1)).player_list.get(0).getName() == "TestName"); //test if player goes up the ladder
         }
 }
